3180b1d0677c882bf1606d0cad0e0982238c4ae6
mnerisilva <marceloneri@gmail.com>
2022-06-07 15:27:04 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+(function(){
+    const _leftBotton = document.querySelector('.leaflet-bottom');
+    _leftBotton.style.pointerEvents = "all";
+    _leftBotton.style.opacity = 0+' !important';
+})();
